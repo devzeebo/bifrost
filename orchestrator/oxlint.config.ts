64cd301adccb08acfd604a235654b4e14b5e3c95
Eric Siebeneich <eric.siebeneich@gmail.com>
2026-05-08 10:35:30 -0500

    update oxlint

--- orchestrator/oxlint.config.ts
+++ orchestrator/oxlint.config.ts
@@ -3,17 +3,27 @@ import { defineConfig } from "oxlint";
 export default defineConfig({
   categories: {
     correctness: "error",
-    suspicious: "error",
-    pedantic: "error",
+    pedantic: "off",
     perf: "error",
-    style: "warn",
     restriction: "error",
+    style: "warn",
+    suspicious: "error",
   },
   plugins: ["typescript"],
   rules: {
+    "capitalized-comments": "off",
+    "max-statements": "off",
     "no-console": "off",
+    "no-magic-numbers": "off",
+    "sort-keys": "off",
+    "no-underscore-dangle": "off",
   },
-  options: {
-    typeAware: true,
-  },
+  overrides: [
+    {
+      files: ["**/*.spec.ts"],
+      rules: {
+        "no-non-null-assertion": "off",
+      },
+    },
+  ],
 });
